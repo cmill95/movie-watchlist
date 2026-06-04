6e9ea7e36a1b6a0ea95acef2808d30a072a7d147
Cole Miller <colemill123@gmail.com>
2026-06-04 16:37:42 -0600

    Select repository backend via movies_backend

--- app/storage/__init__.py
+++ app/storage/__init__.py
@@ -8,14 +8,24 @@ from functools import lru_cache
 
 from app.config import get_settings
 from app.storage.base import MovieRepository
+from app.storage.sqlalchemy_repo import SqlAlchemyMovieRepository
 from app.storage.sqlite_repo import SqliteMovieRepository
 
-__all__ = ["MovieRepository", "SqliteMovieRepository", "get_repository"]
+__all__ = [
+    "MovieRepository",
+    "SqlAlchemyMovieRepository",
+    "SqliteMovieRepository",
+    "get_repository",
+]
 
 
 @lru_cache
 def get_repository() -> MovieRepository:
 
-    repo = SqliteMovieRepository(get_settings().movies_db_path)
+    settings = get_settings()
+    if settings.movies_backend == "sqlalchemy":
+        repo = SqlAlchemyMovieRepository(settings.movies_db_path)
+    else:
+        repo = SqliteMovieRepository(settings.movies_db_path)
     repo.init_schema()
     return repo
